dd365936ef935b89ae45420f4f1dd9fd83f7a782
AJAYSHEOKAND <macbookpro@MACBOOKs-MacBook-Pro.local>
2022-05-24 13:42:20 +0530

    Food App

--- ReactJS/Food_Order/src/components/Cart/Cart.js
+++ ReactJS/Food_Order/src/components/Cart/Cart.js
@@ -1,22 +1,51 @@
 import React from "react";
+import CartItem from "./CartItem";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
 
-export const Cart = (prps) => {
-  const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
-    (item) => <li>{item.name}</li>
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemovehandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemovehandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
   );
   return (
-    <Modal>
-      <cartItems />
+    <Modal onClose={props.onClose}>
+      {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.63</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
-        <button className={classes.order}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.order}>Order</button>}
       </div>
     </Modal>
   );
 };
+
+export default Cart;
--- ReactJS/Food_Order/src/components/UI/Modal.js
+++ ReactJS/Food_Order/src/components/UI/Modal.js
@@ -1,14 +1,18 @@
-import classes from "./Modal.module.css";
 import { Fragment } from "react";
 import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
-  <div className={classes.modal}>
-    <div className={classes.content}> {props.children}</div>
-  </div>;
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
 };
 
 const portalElement = document.getElementById("overlays");
@@ -16,8 +20,14 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />)}
-      {ReactDOM.createPortal(<ModalOverlay> {props.children}</ModalOverlay>)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
     </Fragment>
   );
 };
